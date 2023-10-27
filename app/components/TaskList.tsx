6ec7c2d1d33e7fddc4adbb889bedcc8d696d0748
mdarora <mdarora0001@gmail.com>
2023-10-27 02:18:28 -0400

    showing tasks list

--- app/components/TaskList.tsx
+++ app/components/TaskList.tsx
@@ -1,42 +1,39 @@
-import React from 'react'
+"use client";
+import { getTasks } from '@/api'
+import { TaskContext } from '@/providers/TaskProvider';
+import React, { useContext, useLayoutEffect, useState } from 'react'
 
 const TaskList = () => {
+    const {tasks, setTasks} = useContext(TaskContext);
   return (
     <div className="overflow-x-auto">
+        {tasks.length > 0 ? <>
         <table className="table">
             {/* head */}
             <thead>
             <tr>
                 <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Tasks</th>
+                <th>Mark complete</th>
+                <th>Delete</th>
             </tr>
             </thead>
             <tbody>
-            {/* row 1 */}
-            <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-            </tr>
-            {/* row 2 */}
-            <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-            </tr>
+                {tasks.map((task, i) => (
+                    <tr key={i}>
+                        <th>{i+1}</th>
+                        <td className='w-full'>{task.text}</td>
+                        <td className='text-sky-600'>Mark</td>
+                        <td className='text-red-600'>Delete</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
+
+        </> : <>
+        <h2 className='text-red-700 text-2xl mt-4'>No Tasks Found!</h2>
+        <p>Start adding now</p>
+        </>}
     </div>
   )
 }
